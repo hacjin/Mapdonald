d7964f2f09fe83fe1c75ff0ac0fd626e6b95b602
오석빈 <osb330@naver.com>
2020-04-06 17:15:26 +0900

    S02P22A102-17 유저 모델 뷰 추가
    Former-commit-id: c16800cab340fcf75a29cdf489b26caa2be4e46c

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,8 +13,8 @@ router.register(r"stores", views.StoreViewSet, basename="stores")
 router.register(r'user', views.UserViewSet, basename="user")
 urlpatterns = [
     url(r'swagger-ui.html', schema_view),
-
 ]
+## user url 추가
 ## http://127.0.0.1:8000/api/swagger-ui.html 로 접속하면 가능해짐.
 ## 다음 명령어 실행
 ## python manage.py runserver
