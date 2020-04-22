74cc34b0f8c2a411085a7be2add70dcea381d1ce
root <root@ip-172-26-6-20.ap-northeast-2.compute.internal>
2020-04-22 00:33:12 +0000

    CORS
    Former-commit-id: 959220fbde214d55d243a1b09b61e0d66b7bdfa0

--- front/src/component/KakaoMap.tsx
+++ front/src/component/KakaoMap.tsx
@@ -10,16 +10,17 @@ declare global {
 // 디폴트 역삼역
 const DEFAULT_LATITUDE =  37.501392
 const DEFAULT_LONGITUDE =  127.039648
-
+console.log("전역")
 export default function KakaoMap(){
 
     let map : any;
     const kakao = window.kakao
     useEffect(() => { 
+	console.log(kakao)
         makeMap()
         // 현재 위치정보 권한 요청
         getCurrentPosition()
-    }, [getCurrentPosition])
+    },[])
     
     function getCurrentPosition(){
       navigator.geolocation.getCurrentPosition(geoSuccess)
@@ -66,7 +67,7 @@ export default function KakaoMap(){
 
     // 전달 받은 위도, 경도로 카카오맵을 이동시키는 함수
     function moveMap(latitude : number, longitude : number){  
-        const moveLocation = new window.kakao.maps.LatLng(latitude, longitude)
+        const moveLocation = new kakao.maps.LatLng(latitude, longitude)
         map.panTo(moveLocation)
     }
 
