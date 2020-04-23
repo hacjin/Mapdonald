cb0e2c747124a5467a78c1bfa5c1fc7fb1dcecc8
milkbean <nim950313@gmail.com>
2020-04-23 09:04:03 +0900

    add view
    Former-commit-id: e95d6d64d777ac1ead03616dd4ac185b61895046

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from api import models, serializers
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from haversine import haversine, Unit
 
 
 class SmallPagination(PageNumberPagination):
@@ -31,3 +32,32 @@ class UserViewSet(viewsets.ModelViewSet):
         )
         return queryset
 
+class NearInfoView(View):
+    def get(self, request):
+        try:
+            longitude = float(request.GET.get('longitude', None))
+            latitude  = float(request.GET.get('latitude', None))
+            
+            #몇키로 이내 필터링할건지 값( 입력값으로 받아도 될듯)
+            NearValue = 2
+
+            #포지션이 내 위치
+            position  = (latitude,longitude)
+
+            #1km에 해당하는게 0.01, 0.015라는 의미임
+            condition = (
+                Q(latitude__range  = (latitude - 0.01, latitude + 0.01)) |
+                Q(longitude__range = (longitude - 0.015, longitude + 0.015))
+            )
+
+            store_infos = (
+                storeInfo
+                .objects
+                .filter(condition)
+            )
+
+            
+            near_store_infos = [info for info in store_infos
+                                      if haversine(position, (info.latitude, info.longitude)) <= NearValue]
+
+     
